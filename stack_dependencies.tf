a17642f0f6d6538be64b3f797f6fc22022f3c56e
Mervin Hemaraju <th3pl4gu33@gmail.com>
2024-04-24 20:19:49 +0400

    -

--- stack_dependencies.tf
+++ stack_dependencies.tf
@@ -1,7 +1,27 @@
 
+# resource "spacelift_stack_dependency" "this" {
+#   count = length(var.depends_on_stacks)
+
+#   stack_id            = spacelift_stack.this.id
+#   depends_on_stack_id = var.depends_on_stacks[count.index].depends_on_stack_id
+# }
+
+
+locals {
+  flattened_references = flatten([
+    for stack in var.depends_on_stacks : [
+      for reference in stack.references : {
+        depends_on_stack_id = stack.depends_on_stack_id
+        output_name         = reference.output_name
+        input_name          = reference.input_name
+      }
+    ]
+  ])
+}
+
 resource "spacelift_stack_dependency" "this" {
-  count = length(var.depends_on_stacks)
+  for_each = { for stack in var.depends_on_stacks : stack.depends_on_stack_id => stack }
 
   stack_id            = spacelift_stack.this.id
-  depends_on_stack_id = var.depends_on_stacks[count.index].depends_on_stack_id
+  depends_on_stack_id = each.value.depends_on_stack_id
 }
