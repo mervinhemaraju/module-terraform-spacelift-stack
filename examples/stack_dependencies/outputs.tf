133e6a4ee44d1152515a81eb58b5376ad205dc30
Mervin Hemaraju <th3pl4gu33@gmail.com>
2024-04-24 19:59:14 +0400

    - Create a stack dependency for each map

--- examples/stack_dependencies/outputs.tf
+++ examples/stack_dependencies/outputs.tf
@@ -1,6 +1,6 @@
-output "sd" {
-  value = { for stack in var.depends_on_stacks : stack.depends_on_stack_id => stack }
-}
+# output "sd" {
+#   value = { for stack in var.depends_on_stacks : stack.depends_on_stack_id => stack }
+# }
 
 # output "sdr" {
 #   value = { for stack_id, stack in spacelift_stack_dependency.this : stack_id => stack.references }
--- stack_dependencies.tf
+++ stack_dependencies.tf
@@ -1,37 +1,7 @@
-# resource "spacelift_stack_dependency" "this" {
-#   for_each = { for idx, elem in var.depends_on_stacks : idx => elem }
 
-#   stack_id            = spacelift_stack.this.id
-#   depends_on_stack_id = each.value.depends_on_stack_id
-# }
+resource "spacelift_stack_dependency" "dependency" {
+  count = length(var.depends_on_stacks)
 
-# resource "spacelift_stack_dependency_reference" "this" {
-#   for_each = { for idx, elem in local.references : idx => elem }
-
-#   stack_dependency_id = spacelift_stack_dependency.this[each.value.stack_id].id
-#   output_name         = each.value.output_name
-#   input_name          = each.value.input_name
-# }
-
-# resource "spacelift_stack_dependency_reference" "this" {
-#   for_each = { for elem in local.references : elem.stack_id => elem }
-
-#   stack_dependency_id = spacelift_stack_dependency.this[each.value.stack_id].id
-#   output_name         = each.value.output_name
-#   input_name          = each.value.input_name
-# }
-
-# resource "spacelift_stack_dependency" "this" {
-#   for_each = { for index, stack in var.depends_on_stacks : stack.depends_on_stack_id => stack }
-
-#   stack_id            = spacelift_stack.this.id
-#   depends_on_stack_id = each.key
-# }
-
-# resource "spacelift_stack_dependency_reference" "this" {
-#   for_each = { for stack_id, stack in spacelift_stack_dependency.this : stack_id => stack.references }
-
-#   stack_dependency_id = spacelift_stack_dependency.this[each.key].id
-#   output_name         = each.value.output_name
-#   input_name          = each.value.input_name
-# }
+  stack_id            = spacelift_stack.stack.id
+  depends_on_stack_id = var.dependencies[count.index].depends_on_stack_id
+}
