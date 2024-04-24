1fb1cfe93e570ce8b5578ac3543b8c70538210c5
Mervin Hemaraju <th3pl4gu33@gmail.com>
2024-04-24 20:01:58 +0400

    -

--- stack_dependencies.tf
+++ stack_dependencies.tf
@@ -1,7 +1,7 @@
 
-resource "spacelift_stack_dependency" "dependency" {
+resource "spacelift_stack_dependency" "this" {
   count = length(var.depends_on_stacks)
 
   stack_id            = spacelift_stack.this.id
-  depends_on_stack_id = var.dependencies[count.index].depends_on_stack_id
+  depends_on_stack_id = var.depends_on_stacks[count.index].depends_on_stack_id
 }
