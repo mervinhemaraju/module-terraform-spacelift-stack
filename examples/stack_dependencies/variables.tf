e9f9867b75ad48b5b78a279bc3c318d8fab90c21
Mervin Hemaraju <th3pl4gu33@gmail.com>
2024-04-24 20:00:47 +0400

    -

--- examples/stack_dependencies/variables.tf
+++ examples/stack_dependencies/variables.tf
@@ -1,15 +1,1 @@
-variable "depends_on_stacks" {
-  description = "List of maps containing stack dependencies and references"
-  type        = list(any)
-  default = [
-    {
-      depends_on_stack_id = "id"
-      references = [
-        {
-          output_name = "test1",
-          input_name  = "TF_VAR_test1"
-        }
-      ]
-    }
-  ]
-}
+
--- stack_dependencies.tf
+++ stack_dependencies.tf
@@ -2,6 +2,6 @@
 resource "spacelift_stack_dependency" "dependency" {
   count = length(var.depends_on_stacks)
 
-  stack_id            = spacelift_stack.stack.id
+  stack_id            = spacelift_stack.this.id
   depends_on_stack_id = var.dependencies[count.index].depends_on_stack_id
 }
