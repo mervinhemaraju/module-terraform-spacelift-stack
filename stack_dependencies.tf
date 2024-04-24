7b80465e94cad4858319b19b8550261f9065b714
Mervin Hemaraju <th3pl4gu33@gmail.com>
2024-04-24 19:32:51 +0400

    -

--- stack_dependencies.tf
+++ stack_dependencies.tf
@@ -1,9 +1,9 @@
-resource "spacelift_stack_dependency" "this" {
-  for_each = { for idx, elem in var.depends_on_stacks : idx => elem }
+# resource "spacelift_stack_dependency" "this" {
+#   for_each = { for idx, elem in var.depends_on_stacks : idx => elem }
 
-  stack_id            = spacelift_stack.this.id
-  depends_on_stack_id = each.value.depends_on_stack_id
-}
+#   stack_id            = spacelift_stack.this.id
+#   depends_on_stack_id = each.value.depends_on_stack_id
+# }
 
 # resource "spacelift_stack_dependency_reference" "this" {
 #   for_each = { for idx, elem in local.references : idx => elem }
@@ -13,10 +13,25 @@ resource "spacelift_stack_dependency" "this" {
 #   input_name          = each.value.input_name
 # }
 
-resource "spacelift_stack_dependency_reference" "this" {
-  for_each = { for elem in local.references : elem.stack_id => elem }
+# resource "spacelift_stack_dependency_reference" "this" {
+#   for_each = { for elem in local.references : elem.stack_id => elem }
 
-  stack_dependency_id = spacelift_stack_dependency.this[each.value.stack_id].id
+#   stack_dependency_id = spacelift_stack_dependency.this[each.value.stack_id].id
+#   output_name         = each.value.output_name
+#   input_name          = each.value.input_name
+# }
+
+resource "spacelift_stack_dependency" "this" {
+  for_each = { for stack in var.depends_on_stacks : stack.depends_on_stack_id => stack }
+
+  stack_id            = spacelift_stack.this.id
+  depends_on_stack_id = each.value.depends_on_stack_id
+}
+
+resource "spacelift_stack_dependency_reference" "this" {
+  for_each = { for stack_id, stack in spacelift_stack_dependency.this : stack_id => stack.references }
+
+  stack_dependency_id = spacelift_stack_dependency.this[each.key].id
   output_name         = each.value.output_name
   input_name          = each.value.input_name
 }
